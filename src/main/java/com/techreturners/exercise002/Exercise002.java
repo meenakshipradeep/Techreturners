4fab5a7ff39f159310dad0db161d5080f7f6ea9f
Pradeep <pradeepk.jobs@gmail.com>
2021-09-03 00:51:57 +0100

    Completed Exercise E1 2 3

---
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -0,0 +1,16 @@
+package com.techreturners.exercise002;
+
+public class Exercise002 {
+
+    public boolean isFromManchester(Person person ) {
+        // Add your code here
+        return (person.getCity().equals("Manchester"));
+
+    }
+
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        // Add your code here
+        return (person.getAge() >= ageLimit);
+    }
+    
+}
---
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -0,0 +1,19 @@
+package com.techreturners.exercise003;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise003 {
+    String[] iceCreamFlavours = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+List<String> iceFlavoursList= Arrays.asList(iceCreamFlavours);
+
+    int getIceCreamCode(String iceCreamFlavour) {
+
+        return  iceFlavoursList.indexOf(iceCreamFlavour);
+    }
+
+    String[] iceCreamFlavours() {
+        return iceCreamFlavours;
+    }
+
+}
